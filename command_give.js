485e49a545fca0156e888a9730e40f2b427b0f74
woifdog <105352999+woifdog@users.noreply.github.com>
2022-05-28 20:50:01 +0800

    Update command_give.js

--- command_give.js
+++ command_give.js
@@ -8,7 +8,7 @@ function clickadded(){
     let item = document.getElementById("give_name").value;
     let data = document.getElementById("give_data").value;
     let count = document.getElementById("give_number").value;
-    if (target == "") target = "@s";
+    if (sele_mode == "") sele_mode = "@s";
     if (item == "") return(alert(未填入物品名稱))
     if (count == "") count = "1";
     if (data == "") data = "0";
