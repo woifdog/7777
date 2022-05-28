1728b86f6790f7f4a86bef975445a7e5150adf29
woifdog <105352999+woifdog@users.noreply.github.com>
2022-05-28 20:18:46 +0800

    Update command_give.js

--- command_give.js
+++ command_give.js
@@ -8,19 +8,12 @@ function clickadd(){
     let item = document.getElementById("give_name").value;
     let data = document.getElementById("give_data").value;
     let count = document.getElementById("give_number").value;
-    let give_tag = document.getElementById("give_tag").value;
     let array = "[tag=" + give_tag + "]";
     if (target == "") target = @s
     if (item == "") return(alert("未填入物品名稱"))
     if (count == "") count = "1";
     if (data == "") data = "0";
-    if(give_tag = ""){
-        cmd_text.value = "/give" + " " + target + " " +item + " " + count + " " + data;
-    }
-    else {
-        let array = "[tag=" + give_tag + "]";
-        cmd_text.value = "/give" + " " + target + "" + array + " " +item + " " + count + " " + data;
-    }
+    cmd_text.value = "/give" + " " + target + " " +item + " " + count + " " + data;
 }
 function sele_change(){
     var sele = document.getElementById("sele")
