1301e236e1419c8a5b95ec36dcb32b63cbbd092b
woifdog <105352999+woifdog@users.noreply.github.com>
2022-05-28 20:49:06 +0800

    Update command_give.js

--- command_give.js
+++ command_give.js
@@ -5,7 +5,6 @@ var copy = document.getElementById("copy")
 function clickadded(){
      sele_change()
     let cmd_text = document.getElementById("cmdtext");
-    let target = document.getElementById("sele").value;
     let item = document.getElementById("give_name").value;
     let data = document.getElementById("give_data").value;
     let count = document.getElementById("give_number").value;
@@ -13,7 +12,7 @@ function clickadded(){
     if (item == "") return(alert(未填入物品名稱))
     if (count == "") count = "1";
     if (data == "") data = "0";
-        cmd_text.innerHTML = "give " + target + " " + item + " " + count + " " + data;
+        cmd_text.innerHTML = "give " + sele_mode + " " + item + " " + count + " " + data;
 }
 function sele_change(){
     var sele = document.getElementById("sele")
